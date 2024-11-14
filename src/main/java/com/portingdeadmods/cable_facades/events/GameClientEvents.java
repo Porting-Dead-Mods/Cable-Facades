f745dd459540590cfe3edc261dbda0d9b0e07805
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-11-14 18:45:06 +0100

    added back translucent sorting

--- src/main/java/com/portingdeadmods/cable_facades/events/GameClientEvents.java
+++ src/main/java/com/portingdeadmods/cable_facades/events/GameClientEvents.java
@@ -2,12 +2,14 @@ package com.portingdeadmods.cable_facades.events;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.portingdeadmods.cable_facades.CFMain;
+import com.portingdeadmods.cable_facades.registries.CFItemTags;
 import com.portingdeadmods.cable_facades.registries.CFItems;
 import com.portingdeadmods.cable_facades.utils.TranslucentRenderTypeBuffer;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
@@ -20,48 +22,50 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = CFMain.MODID, value = Dist.CLIENT)
 public final class GameClientEvents {
     public static Map<BlockPos, @Nullable Block> CAMOUFLAGED_BLOCKS = new Object2ObjectOpenHashMap<>();
-
     @SubscribeEvent
     public static void render(RenderLevelStageEvent event) {
         if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_BLOCK_ENTITIES) {
             Minecraft mc = Minecraft.getInstance();
             LocalPlayer player = mc.player;
-
-            for (Map.Entry<BlockPos, Block> entry : CAMOUFLAGED_BLOCKS.entrySet()) {
+            Vec3 cameraPos = event.getCamera().getPosition();
+            BlockPos cameraBlockPos = new BlockPos((int) cameraPos.x(), (int) cameraPos.y(), (int) cameraPos.z());
+            List<Map.Entry<BlockPos, Block>> sortedBlocks = CAMOUFLAGED_BLOCKS.entrySet().stream()
+                    .sorted(Comparator.comparingDouble(entry -> entry.getKey().distSqr(cameraBlockPos)))
+                    .toList();
+            for (Map.Entry<BlockPos, Block> entry : sortedBlocks) {
                 BlockPos blockPos = entry.getKey();
                 Block block = entry.getValue();
                 ClientLevel level = mc.level;
                 BlockState framedBlock = level.getBlockState(blockPos);
-                Vec3 vec3 = event.getCamera().getPosition();
-                double d0 = vec3.x();
-                double d1 = vec3.y();
-                double d2 = vec3.z();
-
                 PoseStack poseStack = event.getPoseStack();
-
                 poseStack.pushPose();
                 {
-                    poseStack.translate((double) blockPos.getX() - d0, (double) blockPos.getY() - d1, (double) blockPos.getZ() - d2);
+                    poseStack.translate(blockPos.getX() - cameraPos.x(), blockPos.getY() - cameraPos.y(), blockPos.getZ() - cameraPos.z());
                     BlockState state = getState(block, framedBlock);
-                    if (!player.getMainHandItem().is(CFItems.WRENCH.get())) {
-                        for (RenderType type : mc.getBlockRenderer().getBlockModel(state).getRenderTypes(state, mc.level.random, ModelData.EMPTY)) {
-                            mc.getBlockRenderer().renderBatched(state, blockPos, mc.level, poseStack, mc.renderBuffers().bufferSource().getBuffer(type), true, mc.level.random, ModelData.EMPTY, type);
-                        }
-                    } else {
-                        for (RenderType type : mc.getBlockRenderer().getBlockModel(state).getRenderTypes(state, mc.level.random, ModelData.EMPTY)) {
+                    MultiBufferSource.BufferSource bufferSource = mc.renderBuffers().bufferSource();
+                    if(mc.player.getMainHandItem().getTags().anyMatch(itemTag -> itemTag.equals(CFItemTags.WRENCHES))){
+                        for (RenderType type : mc.getBlockRenderer().getBlockModel(state).getRenderTypes(state,mc.level.random,ModelData.EMPTY)){
                             mc.getBlockRenderer().renderBatched(state, blockPos, mc.level, poseStack, new TranslucentRenderTypeBuffer(mc.renderBuffers().bufferSource(), 120).getBuffer(type), true, mc.level.random, ModelData.EMPTY, type);
                         }
+                    }else{
+                        for (RenderType type : mc.getBlockRenderer().getBlockModel(state).getRenderTypes(state, mc.level.random, ModelData.EMPTY)) {
+                            mc.getBlockRenderer().renderBatched(state, blockPos, mc.level, poseStack, bufferSource.getBuffer(type), true, mc.level.random, ModelData.EMPTY, type);
+                        }
                     }
+                    bufferSource.endBatch();
                 }
                 poseStack.popPose();
             }
         }
     }
+
 
     private static BlockState getState(Block block, BlockState framedBlock) {
         return block != null ? block.defaultBlockState() : framedBlock;
