7eb64e013293a0948544a339124d6cf8fc671c59
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-11-16 20:28:10 +0100

    optimisations

--- src/main/java/com/portingdeadmods/cable_facades/mixins/BlockStateMixin.java
+++ src/main/java/com/portingdeadmods/cable_facades/mixins/BlockStateMixin.java
@@ -1,7 +1,6 @@
 package com.portingdeadmods.cable_facades.mixins;
 
 import com.portingdeadmods.cable_facades.events.ClientCamoManager;
-import com.portingdeadmods.cable_facades.events.GameClientEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -11,7 +10,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.extensions.IForgeBlockState;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(BlockState.class)
@@ -19,33 +17,34 @@ public abstract class BlockStateMixin implements IForgeBlockState {
 
     @Override
     public BlockState getAppearance(BlockAndTintGetter level, BlockPos pos, Direction side, @Nullable BlockState queryState, @Nullable BlockPos queryPos) {
-        if (ClientCamoManager.CAMOUFLAGED_BLOCKS.containsKey(pos)) {
-            Block camoBlock = ClientCamoManager.CAMOUFLAGED_BLOCKS.get(pos);
-            if (camoBlock != null) {
-                BlockState camoState = camoBlock.defaultBlockState();
-                return camoState.getBlock().getAppearance(camoState, level, pos, side, queryState, queryPos);
-            }
+        Block camoBlock = ClientCamoManager.CAMOUFLAGED_BLOCKS.get(pos);
+        if (camoBlock != null) {
+            BlockState camoState = getCamoState(camoBlock);
+            return camoState.getBlock().getAppearance(camoState, level, pos, side, queryState, queryPos);
         }
-        return cable_facades$self().getBlock().getAppearance(cable_facades$self(), level, pos, side, queryState, queryPos);
-    }
 
+        BlockState self = cable_facades$self();
+        return self.getBlock().getAppearance(self, level, pos, side, queryState, queryPos);
+    }
 
     @Override
     public int getLightEmission(BlockGetter level, BlockPos pos) {
-
-        if (ClientCamoManager.CAMOUFLAGED_BLOCKS.containsKey(pos)) {
-            Block camoBlock = ClientCamoManager.CAMOUFLAGED_BLOCKS.get(pos);
-            if (camoBlock != null) {
-                BlockState camoState = camoBlock.defaultBlockState();
-                return camoState.getLightEmission();
-            }
+        Block camoBlock = ClientCamoManager.CAMOUFLAGED_BLOCKS.get(pos);
+        if (camoBlock != null) {
+            return getCamoState(camoBlock).getLightEmission();
         }
-        return cable_facades$self().getBlock().getLightEmission(cable_facades$self(), level, pos);
-    }
 
+        BlockState self = cable_facades$self();
+        return self.getBlock().getLightEmission(self, level, pos);
+    }
 
     @Unique
     private BlockState cable_facades$self() {
         return (BlockState) (Object) this;
+    }
+
+    @Unique
+    private static BlockState getCamoState(Block block) {
+        return block.defaultBlockState();
     }
 }
