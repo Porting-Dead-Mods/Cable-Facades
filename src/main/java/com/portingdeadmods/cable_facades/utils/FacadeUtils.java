7776567dc97cf56998e4288c331ddc40c92a3020
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-11-17 00:57:03 +0100

    Fix fabulous rendering

--- src/main/java/com/portingdeadmods/cable_facades/utils/FacadeUtils.java
+++ src/main/java/com/portingdeadmods/cable_facades/utils/FacadeUtils.java
@@ -2,11 +2,9 @@ package com.portingdeadmods.cable_facades.utils;
 
 import com.portingdeadmods.cable_facades.data.CableFacadeSavedData;
 import com.portingdeadmods.cable_facades.events.ClientCamoManager;
-import com.portingdeadmods.cable_facades.events.GameClientEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
 public class FacadeUtils {
