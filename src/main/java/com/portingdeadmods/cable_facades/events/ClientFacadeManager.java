c24049e134fa4d1ec3556b0b1630080e35d95301
IMS212 <ims@irisshaders.dev>
2024-12-24 08:18:11 -0800

    Improve culling behavior

--- src/main/java/com/portingdeadmods/cable_facades/events/ClientFacadeManager.java
+++ src/main/java/com/portingdeadmods/cable_facades/events/ClientFacadeManager.java
@@ -1,5 +1,6 @@
 package com.portingdeadmods.cable_facades.events;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,7 +13,7 @@ import java.util.Map;
 
 public final class ClientFacadeManager {
     // Blocks in the currently loaded chunks. Updated whenever a chunk is loaded or unloaded by this player
-    public static Map<BlockPos, @Nullable BlockState> FACADED_BLOCKS = new HashMap<>();
+    public static Map<BlockPos, @Nullable BlockState> FACADED_BLOCKS = new Object2ObjectOpenHashMap<>();
     // Map to keep track of what blockpositions are in which chunks
-    public static final Map<ChunkPos, List<BlockPos>> LOADED_BLOCKS = new HashMap<>();
+    public static final Map<ChunkPos, List<BlockPos>> LOADED_BLOCKS = new Object2ObjectOpenHashMap<>();
 }
