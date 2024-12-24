0d506a8a569b75308cbc247a8a9737c8ffd46f26
IMS212 <ims@irisshaders.dev>
2024-12-24 07:22:17 -0800

    Rewrite to add to section geometry

--- src/main/java/com/portingdeadmods/cable_facades/registries/CFItems.java
+++ src/main/java/com/portingdeadmods/cable_facades/registries/CFItems.java
@@ -2,7 +2,13 @@ package com.portingdeadmods.cable_facades.registries;
 
 import com.portingdeadmods.cable_facades.CFMain;
 import com.portingdeadmods.cable_facades.content.items.FacadeItem;
+import com.portingdeadmods.cable_facades.events.GameClientEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -16,5 +22,14 @@ public class CFItems {
             .component(CFDataComponents.FACADE_BLOCK, Optional.empty())
             .component(CFDataComponents.HAS_FACADE_REMAINDER, false)));
 
-    public static final DeferredItem<Item> WRENCH = ITEMS.register("facade_wrench", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> WRENCH = ITEMS.register("facade_wrench", () -> new Item(new Item.Properties().stacksTo(1)) {
+        @Override
+        public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+            if (level.isClientSide) {
+                GameClientEvents.facadeTransparency = !GameClientEvents.facadeTransparency;
+            }
+
+            return super.use(level, player, usedHand);
+        }
+    });
 }
